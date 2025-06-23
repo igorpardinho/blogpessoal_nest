894f1c626cbb54cc44ba0223e0f2eef6180623a8
Igor Pardinho <pardinhoigor@gmail.com>
2025-06-23 19:32:33 -0300

    feat: controller e service de postagem

--- src/postagem/controller/postagem.controller.ts
+++ src/postagem/controller/postagem.controller.ts
@@ -1,4 +1,12 @@
-import { Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { PostagemService } from '../services/postagem.service';
 import { Postagem } from '../entities/postagem.entity';
 
@@ -9,5 +17,23 @@ export class PostagemController {
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  save(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.save(postagem);
+  }
+
+  @Get('/{id}')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param() postagemId: number): Promise<Postagem | null> {
+    return this.postagemService.findById(postagemId);
+  }
+
+  @Delete('/{id}')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(postagemId: number): Promise<void> {
+    return this.postagemService.remove(postagemId);
   }
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -13,4 +13,19 @@ export class PostagemService {
   async findAll(): Promise<Postagem[]> {
     return await this.postagemRepository.find();
   }
+
+  async save(postagem: Postagem): Promise<Postagem> {
+    return await this.postagemRepository.save(postagem);
+  }
+
+  async findById(postagemId: number): Promise<Postagem | null> {
+    return await this.postagemRepository.findOne({ where: { id: postagemId } });
+  }
+
+  async remove(postagemId: number): Promise<void> {
+    const postagem = await this.findById(postagemId);
+    if (postagem) {
+      await this.postagemRepository.delete(postagemId);
+    }
+  }
 }

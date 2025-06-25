03ca69b326c265e4bbb5845019b10f02a36c4ebb
Igor Pardinho <pardinhoigor@gmail.com>
2025-06-25 16:45:04 -0300

    feat: cria os metodos do service de Tema

--- src/tema/services/tema.service.ts
+++ src/tema/services/tema.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { TemaDtoRequest } from './../dtos/tema.dto.request';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ILike, Repository } from 'typeorm';
 import { Tema } from '../entities/tema.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -9,4 +10,58 @@ export class TemaService {
     @InjectRepository(Tema)
     private readonly temaRepository: Repository<Tema>,
   ) {}
+
+  async findAll(): Promise<Tema[]> {
+    return this.temaRepository.find({
+      relations: {
+        postagem: true,
+      },
+    });
+  }
+
+  async save(dto: TemaDtoRequest): Promise<Tema> {
+    const { descricao } = dto;
+
+    const novoTema = this.temaRepository.create({
+      descricao,
+    });
+
+    return await this.temaRepository.save(novoTema);
+  }
+
+  async findById(temaId: number): Promise<Tema> {
+    const tema = await this.temaRepository.findOne({ where: { id: temaId } });
+
+    if (!tema) {
+      throw new NotFoundException('tema não encontrado');
+    }
+    return tema;
+  }
+
+  async update(temaId: number, dto: TemaDtoRequest): Promise<Tema> {
+    const tema = await this.findById(temaId);
+
+    if (!tema) {
+      throw new NotFoundException('tema não encontrado');
+    }
+    tema.descricao = dto.descricao;
+    return await this.temaRepository.save(tema);
+  }
+
+  async remove(temaId: number): Promise<void> {
+    const tema = await this.findById(temaId);
+
+    if (!tema) {
+      throw new NotFoundException('tema não encontrado');
+    }
+    await this.temaRepository.delete(temaId);
+  }
+
+  async findByDescricao(descricao: string): Promise<Tema[]> {
+    return await this.temaRepository.find({
+      where: {
+        descricao: ILike(`%${descricao}%`),
+      },
+    });
+  }
 }
